9bfd5bef838adb98b155d90b805f61b810d31ce4
baninho <florianimbt@gmail.com>
2022-07-07 14:29:47 +0200

    add routes

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import { Layout } from '../components/Layout'
 
-const Home: NextPage = () => {
+const Blog: NextPage = () => {
   return (
-  <Layout title="Florian Imbt" page="home">
+  <Layout title="Blog - Florian Imbt" page="blog">
     <div className="container p-24">
       <h1 className="font-serif text-green-2 font-bold text-6xl">Florian Imbt</h1>
       <h2 className="font-sans text-orange-700 font-bold text-5xl italic">Braunschweig</h2>
@@ -14,4 +14,4 @@ const Home: NextPage = () => {
   </Layout>  )
 }
 
-export default Home
+export default Blog
--- pages/connect.tsx
+++ pages/connect.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from 'next'
 import { Layout } from '../components/Layout'
 
-const Home: NextPage = () => {
+const Connect: NextPage = () => {
   return (
-  <Layout title="Florian Imbt" page="home">
+  <Layout title="Kontakt - Florian Imbt" page="connect">
     <div className="container p-24">
       <h1 className="font-serif text-green-2 font-bold text-6xl">Florian Imbt</h1>
       <h2 className="font-sans text-orange-700 font-bold text-5xl italic">Braunschweig</h2>
@@ -14,4 +14,4 @@ const Home: NextPage = () => {
   </Layout>  )
 }
 
-export default Home
+export default Connect
